28b8793b2083ab484c0e663e8f7dbe6a07f18edb
username <wnt925@gmail.com>
2019-11-02 10:00:16 -0600

    Uncomment line in routes and repush to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
   namespace :v1 do
-    # get "/olympians", to: "olympians#index"
+    get "/olympians", to: "olympians#index"
     get "/olympians", to: "olympians#show"
     end
   end
-  end
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
