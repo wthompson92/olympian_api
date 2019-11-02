34832cf174c3327af15cacedbb08c6ba2fec73a5
username <wnt925@gmail.com>
2019-11-02 09:32:15 -0600

    Add active record queries to raw datum model

--- app/models/raw_datum.rb
+++ app/models/raw_datum.rb
@@ -5,6 +5,45 @@ class RawDatum < ApplicationRecord
      select('distinct on (name) *')
   end
 
-  
+  def self.oldest_olympians
+    order("age DESC").first
+  end
+
+  def self.youngest_olympian
+     # select('distinct on (name) *')
+     order("age DESC").last
+  end
+
+  def number_of_olympians
+    list_of_olympians.count
+  end
+
+  def self.female_average_weight
+    select('distinct on (name) *')
+    .where(sex: "F")
+    .average(:weight)
+  end
+
+
+  def self.male_average_weight
+    select('distinct on (name) *')
+    .where(sex: "M")
+    .average(:weight)
+  end
+
+  def self.average_age
+    select('distinct on (name) *')
+    .average(:age)
+  end
+
+
+  # select('distinct on (name) *')
+
+  # def olympic_medals
+  #   gold = RawDatum.where(medal: "Gold")
+  #   silver = RawDatum.where(medal: "Silver")
+  #   bronze = RawDatum.where(medal: "Bronze")
+  #
+  # end
 
 end
