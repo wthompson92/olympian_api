758d97b06796c313cc373e18aef57c1f726ad6e9
username <wnt925@gmail.com>
2019-11-02 16:10:07 -0600

    Manually create stat json response

--- app/controllers/api/v1/stats_controller.rb
+++ app/controllers/api/v1/stats_controller.rb
@@ -5,7 +5,13 @@ class Api::V1::StatsController < ApplicationController
     avg_age = RawDatum.average_age.round
     avg_female_weight = RawDatum.avg_female_weight.round
     avg_male_weight = RawDatum.avg_male_weight.round
-  render json: StatSerializer.new(number_of_olympians, avg_age,  avg_female_weight, avg_male_weight)
-  binding.pry
+
+    render json: {
+      olympian_stats:
+      { total_competing_olympians: number_of_olympians,     average_weight:
+        {unit: "kg",
+          male_olympians: avg_male_weight,
+          female_olympians: avg_female_weight},
+          average_age: avg_age}}
   end
 end
